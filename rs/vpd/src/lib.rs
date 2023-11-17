2912c938a5c836c97ccd597ebc418a0cf6a8967b
twystd <twystd@users.noreply.github.com>
2023-11-16 22:12:36 -0800

    Reworked rust project to target web directly

--- rs/vpd/src/lib.rs
+++ rs/vpd/src/lib.rs
@@ -2,6 +2,21 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
+
+
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
@@ -10,4 +25,9 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, vpd!");
+}
+
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
 }
