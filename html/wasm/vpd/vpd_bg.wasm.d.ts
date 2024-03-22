838fff841423720ebd0724b68477d6805cdc8ffd
twystd <twystd@users.noreply.github.com>
2024-03-21 17:58:11 -0700

    Added VPD wasm files back

---
+++ html/wasm/vpd/vpd_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function restore(): number;
+export function exec(a: number, b: number): number;
+export function undo(): number;
+export function serialize(a: number, b: number, c: number): void;
+export function clear(a: number): void;
+export function load(a: number, b: number, c: number): void;
+export function render(a: number, b: number, c: number): void;
+export function query(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h93af7b67b4e8a16c(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h4945ea6cc9f3371f(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_start(): void;
